bbab33b504e92716591d2c467aa3671d94785986
Corentin Teillet <teillet.corentin@neuf.fr>
2022-02-06 02:38:24 +0100

    avancement back

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from .views import BooksView, SearchBook
+from .views import BooksView, SearchBook, AdvancedSearchBook, SearchBookByLanguage, AddBookClick, SearchByTitle, \
+    LanguageView, ShowBook
 
 urlpatterns = [
     path('', BooksView.as_view(), name='books'),
-    path('search', SearchBook.as_view(), name='search'),
+    path('search/<str:word_req>', SearchBook.as_view(), name='search'),
+    path('advanced-search/<str:expression>', AdvancedSearchBook.as_view(), name='advanced-search'),
+    path('search-language/<str:language>', SearchBookByLanguage.as_view(), name='search-language'),
+    path('add-click/<int:book_id>', AddBookClick.as_view(), name='add-click'),
+    path('search-title/<str:title>', SearchByTitle.as_view(), name='search-title'),
+    path('languages', LanguageView.as_view(), name='languages'),
+    path('<int:book_id>', ShowBook.as_view(), name='book-detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -10,7 +10,7 @@ from research_RegEx.regex import generate_dfa
 
 class BooksView(APIView):
     def get(self, request):
-        res = serializers.serialize('json', Book.objects.all(), cls=LazyEncoder)
+        res = serializers.serialize('json', Book.objects.filter(downloaded=True), cls=LazyEncoder)
         return JsonResponse(res, safe=False)
 
 
@@ -22,8 +22,7 @@ class LazyEncoder(DjangoJSONEncoder):
 
 
 class SearchBook(APIView):
-    def get(self, request):
-        word_req = request.GET.get('word')
+    def get(self, request, word_req):
         res_words = Words.objects.filter(word=word_req)  # index of the word in the table Words
         res = set()
         for word in res_words:
@@ -36,16 +35,14 @@ class SearchBook(APIView):
 
 
 class SearchBookByLanguage(APIView):
-    def get(self, request):
-        language_req = request.GET.get('language')
-        res = Book.objects.filter(language=language_req)
+    def get(self, request, language):
+        res = Book.objects.filter(language=language, downloaded=True)
         json = serializers.serialize('json', res, cls=LazyEncoder)
         return JsonResponse(json, safe=False)
 
 
 class AdvancedSearchBook(APIView):
-    def get(self, request):
-        expression = request.GET.get('expression')
+    def get(self, request, expression):
         if contains_only_letters_or_digit(expression):
             res_words = Words.objects.filter(word__contains=expression)  # id of the words that contain the word
             res = set()
@@ -56,7 +53,6 @@ class AdvancedSearchBook(APIView):
                     res.add(b)
             json = serializers.serialize('json', res, cls=LazyEncoder)
             return JsonResponse(json, safe=False)
-
         else:
             words = list(Words.objects.values_list('word', flat=True))
             dfa = generate_dfa(expression)
@@ -79,9 +75,8 @@ def contains_only_letters_or_digit(word):
 
 
 class AddBookClick(APIView):
-    def post(self, request):
-        id_book = request.GET.get('id')
-        book = Book.objects.get(gutenbergID=id_book)
+    def post(self, request, book_id):
+        book = Book.objects.get(gutenbergID=book_id)
         book.clicks += 1
         book.save()
         return JsonResponse({'clicks': book.clicks})
@@ -97,9 +92,18 @@ class SearchByTitle(APIView):
 
 class LanguageView(APIView):
     def get(self, request):
-        languages_list = Book.objects.values_list('language', flat=True).distinct()
+        languages_list = Book.objects.values_list('language', flat=True).distinct().order_by()
+        print(languages_list)
         res_list = []
         for language in languages_list:
-            res_list.append((languages.get(language=language).name, language))
-        res = serializers.serialize('json', res_list, cls=LazyEncoder)
+            if len(language) == 2:
+                l: languages = languages.get(alpha2=language)
+                res_list.append((l.name, language))
+        return JsonResponse(res_list, safe=False)
+
+
+class ShowBook(APIView):
+    def get(self, request, book_id):
+        book = Book.objects.get(gutenbergID=book_id)
+        res = serializers.serialize('json', [book], cls=LazyEncoder)
         return JsonResponse(res, safe=False)
